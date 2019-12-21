b9a9566df495224a5be0cb40c0d9612058dc4526
Maayaavi <jeyasiri.lavaniyan@gmail.com>
2019-12-21 16:48:26 +0100

    loops are corrected

--- main.py
+++ main.py
@@ -28,9 +28,8 @@ class Game:
         self.while_start = True
         self.while_intro = True
         self.while_game = True
-        self.while_lost = True
+        self.while_lose = True
         self.while_win = True
-        print(self.data_item)
 
     def main(self):
         pygame.init()
@@ -85,6 +84,7 @@ class Game:
                             level_item.display_ether()
                             level_item.display_needle()
                             level_item.display_tube()
+                            # level_item.display_item()
 
                             # Generate the character
                             mg = Character(level)
@@ -93,6 +93,7 @@ class Game:
                             level_display = Display(level, self.data_item)
                             level_display.display_game()
 
+                            # Stop intro loop
                             self.while_intro = False
                             self.while_start = False
 
@@ -106,30 +107,22 @@ class Game:
                 # If the user leaves, we put the variable that continues the game
                 # AND the general variable to 0 to close the window
                 if event.type == QUIT:
+
+                    # Stop game loop
                     self.while_game = False
 
-                    # Reset data value
-                    self.data_item = {"e": 0, "n": 0, "t": 0}
                 elif event.type == KEYDOWN:
                     # If the user press Esc, we just go back to the menu
                     if event.key == K_ESCAPE:
-                        pygame.mixer.stop()
+
+                        # Reinitialize an object
+                        self.reset()
+
+                        # Go to the main loop
+                        self.main()
 
                         # Close the game loop
                         self.while_game = False
-                        # Reinitialize an object
-                        self.reset()
-                        # Go to the main loop
-                        self.main()
-                        # Reinitialize value
-                        self.data_item = {"e": 0, "n": 0, "t": 0}
-
-                        print("self.while_game" + str(self.while_game))
-                        print("self.while_intro" + str(self.while_intro))
-                        print("self.while_start" + str(self.while_start))
-                        print("self.while_win" + str(self.while_win))
-                        print("self.while_lost" + str(self.while_lost))
-                        print(self.data_item)
 
                     # Character Move Keys
                     elif event.key == K_RIGHT:
@@ -151,61 +144,62 @@ class Game:
             pygame.display.flip()
 
             if mg.exit() == 'lost':
-                # Reset data value
-                self.data_item = {"e": 0, "n": 0, "t": 0}
-                # Stop game music
-                pygame.mixer.stop()
-                # play lost sound
+                # Play lost sound
                 pygame.mixer.Sound(sound_lost).play()
 
                 # Lost loop
-                while self.while_lost:
+                while self.while_lose:
                     pygame.time.Clock().tick(30)
                     for event in pygame.event.get():
                         if event.type == KEYDOWN and event.key == K_RETURN:
-                            self.while_lost = False
-                            self.while_game = False
-                        elif event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-                            self.while_lost = False
+
+                            self.reset()
+                            self.main()
+                            self.while_lose = False
                             self.while_game = False
 
-                    # Loading and viewing the lost screen
+                        elif event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+
+                            self.while_lose = False
+                            self.while_game = False
+
+                    # Diplay the lost screen
                     lost = pygame.image.load(image_lost).convert()
                     self.window.blit(lost, (0, 0))
                     pygame.display.flip()
 
             elif mg.exit() == 'win':
-                self.data_item = {"e": 0, "n": 0, "t": 0}
-                # Stop game music
-                pygame.mixer.stop()
-                # Start win sound
+                # Play win sound
                 pygame.mixer.Sound(sound_win).play()
                 # Victory loop
                 while self.while_win:
                     pygame.time.Clock().tick(30)
                     for event in pygame.event.get():
                         if event.type == KEYDOWN and event.key == K_RETURN:
-                            self.while_win = False
-                            self.while_game = False
-                            self.while_intro = False
-                            self.while_start = False
-                        elif event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-                            self.while_win = False
-                            self.while_game = False
-                            self.while_intro = False
-                            self.while_start = False
 
-                    # Loading and viewing the victory screen
+                            self.reset()
+                            self.main()
+                            self.while_win = False
+                            self.while_game = False
+
+                        elif event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+
+                            self.while_win = False
+                            self.while_game = False
+
+                    # Display the victory screen
                     win = pygame.image.load(image_win).convert()
                     self.window.blit(win, (0, 0))
                     pygame.display.flip()
 
     def reset(self):
+        """ Method for Reinitialize value """
         self.while_start = True
         self.while_intro = True
         self.while_game = True
-        self.while_lost = True
+        self.while_lose = True
         self.while_win = True
+        self.data_item = {"e": 0, "n": 0, "t": 0}
 
 
 if __name__ == "__main__":

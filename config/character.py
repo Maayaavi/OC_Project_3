7daa587db62c494e823ea14757ece7857a00d003
Maayaavi <jeyasiri.lavaniyan@gmail.com>
2019-12-22 14:19:13 +0100

    module and package refined

---
+++ config/character.py
@@ -0,0 +1,75 @@
+import pygame
+from utils.constantes import *
+
+
+class Character:
+    """Class to create a character"""
+    def __init__(self, level, item):
+        # Sprite of the character
+        self.character = pygame.image.load(image_character).convert_alpha()
+        # Character position in boxes and pixels
+        self.case_x = 1
+        self.case_y = 0
+        self.x = 30
+        self.y = 0
+        self.data_item = item
+        self.niveau = level
+
+    def move_right(self):
+        """Method for move the character to the right"""
+        # Not to exceed the screen
+        if self.case_x < (number_sprite_side - 1):
+            # Check that the destination box is not a wall
+            if self.niveau.structure[self.case_y][self.case_x + 1] not in ['-', '+',  '|']:
+                self.case_x += 1
+                # Calculation of the "real" position in pixel
+                self.x = self.case_x * size_sprite
+
+    def move_left(self):
+        """Method for move the character to the left"""
+        if self.case_x > 0:
+            if self.niveau.structure[self.case_y][self.case_x - 1] not in ['-', '+', '|']:
+                self.case_x -= 1
+                self.x = self.case_x * size_sprite
+
+    def move_up(self):
+        """Method for move up the character"""
+        if self.case_y > 0:
+            if self.niveau.structure[self.case_y - 1][self.case_x] not in ['-', '+', '|']:
+                self.case_y -= 1
+                self.y = self.case_y * size_sprite
+
+    def move_down(self):
+        """Method for move down the character"""
+        if self.case_y < (number_sprite_side - 1):
+            if self.niveau.structure[self.case_y + 1][self.case_x] not in ['-', '+', '|']:
+                self.case_y += 1
+                self.y = self.case_y * size_sprite
+
+    def find(self):
+        """Method for find items"""
+        # If the player go into the items it will be on the dictionary
+        if self.niveau.structure[self.case_y][self.case_x] in '1':
+            self.data_item["e"] = 1
+            # Delete the item
+            self.niveau.structure[self.case_y][self.case_x] = " "
+            print(self.data_item)
+        elif self.niveau.structure[self.case_y][self.case_x] in '2':
+            self.data_item["n"] = 1
+            self.niveau.structure[self.case_y][self.case_x] = " "
+            print(self.data_item)
+        elif self.niveau.structure[self.case_y][self.case_x] in '3':
+            self.data_item["t"] = 1
+            print(self.data_item)
+            self.niveau.structure[self.case_y][self.case_x] = " "
+
+    def exit(self):
+        """Method for exit from the labyrinth"""
+        if self.niveau.structure[self.case_y][self.case_x] in 'g':
+            if self.data_item == {"e": 1, "n": 1, "t": 1}:
+                # Transform the guardian to sleeping
+                self.niveau.structure[self.case_y][self.case_x] = "s"
+            else:
+                return 'lost'
+        elif self.niveau.structure[self.case_y][self.case_x] == 'a':
+            return 'win'
---
+++ config/item.py
@@ -0,0 +1,49 @@
+from random import *
+
+
+class Items:
+    """Describes an item"""
+    def __init__(self, level):
+        # Items position in boxes
+        self.case_x = 0
+        self.case_y = 0
+        self.i = None
+        # Level in which the items is located
+        self.level = level
+
+    def display_item(self):
+        """Method for display all items"""
+        self.display_ether()
+        self.display_needle()
+        self.display_tube()
+
+    def random_case(self):
+        """Method to get a random case"""
+        self.case_x = randint(1, 14)
+        self.case_y = randint(1, 14)
+        self.i = self.level.structure[self.case_x][self.case_y]
+
+    def display_ether(self):
+        """Method for display randomly ether"""
+        while True:
+            self.random_case()
+            if self.i in ' ':
+                self.level.structure[self.case_x][self.case_y] = '1'
+                break
+
+    def display_needle(self):
+        """Method for display randomly needle"""
+        while True:
+            self.random_case()
+            if self.i in ' ':
+                self.level.structure[self.case_x][self.case_y] = '2'
+                break
+
+    def display_tube(self):
+        """Method for display randomly tube"""
+        while True:
+            self.random_case()
+            if self.i in ' ':
+                self.level.structure[self.case_x][self.case_y] = '3'
+                break
+
